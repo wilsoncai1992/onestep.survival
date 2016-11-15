c4a5a111894fbd5a9f4deed7949f422ed3237773
Wilson Cai <wcai@berkeley.edu>
2016-11-14 19:36:52 -0800

    plot method

--- R/plot.one.step.surv.R
+++ R/plot.one.step.surv.R
@@ -11,9 +11,9 @@
 #' @examples
 plot.onestep.surv <- function(onestepfit, col = 'green', lty = 1, add = FALSE) {
 	if (add){
-		lines(onestepfit[[1]] ~ onestepfit[[2]], col = col, lty = lty)
+		lines(onestepfit$Psi.hat ~ onestepfit$T.uniq, col = col, lty = lty)
 	}else{
-		plot(onestepfit[[1]] ~ onestepfit[[2]], col = col, type = 'l', lty = lty)
+		plot(onestepfit$Psi.hat ~ onestepfit$T.uniq, col = col, type = 'l', lty = lty)
 	}
 
 }
