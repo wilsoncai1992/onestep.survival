1995a417cf7a07a66113500c21ff37335312c537
Wilson Cai <wcai@berkeley.edu>
2016-11-27 16:50:58 -0800

    binning continuous W

--- R/check_and_preprocess.R
+++ R/check_and_preprocess.R
@@ -2,21 +2,21 @@
 #'
 #' @param      dat   The dat
 #' @param      dW    The d w
+#' @param      nbin  how many levels should continuous W's be binned into
 #'
 #' @return
 #' @export
-check_and_preprocess <- function(dat, dW) {
-
+#'
+check_and_preprocess <- function(dat, dW, nbin = 4) {
     to_keep <- (dat$T.tilde != 0) & (dat$T.tilde != max(dat$T.tilde))
     dW <- dW[to_keep]
     dat <- dat[to_keep,]
 
     n.data <- nrow(dat)
-
     W_names <- grep('W', colnames(dat), value = TRUE)
-    # ================================================================================================
+    # ==================================================================================
     # input validation
-    # ================================================================================================
+    # ==================================================================================
     if (length(dW) != n.data) {
         stop('The length of input dW is not same as the sample size!')
     }
@@ -37,7 +37,38 @@ check_and_preprocess <- function(dat, dW) {
         # if there are no T.tilde
         stop("There should be T.tilde variable!")
     }
+    # ==================================================================================
+    # continuous W: binning
+    # ==================================================================================
+    checkBinary <- function(v, naVal="NA") {
+        if (!is.numeric(v)) stop("Only numeric vectors are accepted.")
 
+        vSet = unique(v)
+        if (!missing(naVal)) vSet[vSet == naVal] = NA
+        vSet = vSet[!is.na(vSet)]
+
+        if (any(as.integer(vSet) != vSet)) "con"
+        else if (length(vSet) > 2) "con"
+        else "bin"
+    }
+    checkBinary_df <- function(df, W_names) {
+        all_out <- character()
+        for (it in W_names) {
+            all_out <- c(all_out, checkBinary(v = df[,it]))
+        }
+        return(all_out)
+    }
+    is_conti <- checkBinary_df(df = dat, W_names = W_names) != 'bin'
+    W_conti <- W_names[is_conti]
+
+    if(any(is_conti)) message(paste("Binning continuous covariates:", W_conti, collapse = ','))
+    for (it in W_conti) {
+        dat[,it] <- as.numeric(cut(dat[,it], nbin))
+    }
+    # ==================================================================================
+    # check for positivity
+    # ==================================================================================
+    check_positivity(dat = dat, posit_level = 0.05)
 
     return(list(dat = dat, dW = dW, n.data = n.data, W_names = W_names))
 }
