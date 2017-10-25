def5cd2a19b0e73d49241a132d4a74a1cfe06853
Wilson Cai <wcai@berkeley.edu>
2017-10-24 23:30:04 -0700

    combine functions into single files

--- R/check_and_preprocess.R
+++ R/check_and_preprocess.R
@@ -84,3 +84,40 @@ check_and_preprocess_data <- function(dat, dW, nbin = 4, T.cutoff = NULL) {
 
     return(list(dat = dat, dW = dW, n.data = n.data, W_names = W_names))
 }
+
+
+#' Check positivity assumption of input data
+#'
+#' Check whether positivity assumption is violated in data
+#' censored data NOT supported
+#'
+#' @param dat input data.frame
+#' @param posit_level level of positivity to detect from data
+#'
+#' @return raise warning when positivity violated
+#' @export
+#'
+#' @examples
+check_positivity <- function(dat, posit_level = 0.05) {
+    W_names <- grep('W', colnames(dat), value = TRUE)
+    W <- dat[,W_names]
+    A <- dat[,'A']
+
+    table_out <- ftable(cbind(W,A))
+    if (is.null(dim(W))) {
+        # if W is vector
+        table_out <- table(data.frame(cbind( W,A)))
+    }
+    table_out <- table_out/rowSums(table_out)
+    # there are strata where there is NO observation
+    table_out <- table_out[complete.cases(table_out),]
+    # table_out[is.na(table_out)] <- 0
+
+    posit_violate_by_strata <- rowSums(table_out < posit_level)
+    # some strata have absolutely no entry
+    # posit_violate_by_strata[posit_violate_by_strata == 2] <- 1
+
+    if(any(as.logical(posit_violate_by_strata))){
+        warning(paste('Positivity assumption violated', sum(posit_violate_by_strata), 'out of', length(posit_violate_by_strata), 'stratas!'))
+    }
+}
--- R/initial_SL_fit.R
+++ R/initial_SL_fit.R
@@ -1,3 +1,181 @@
+#' wrapper for conditonal hazard regression SuperLearner
+#'
+#' use T, A, C, W data format as input
+#'
+#' @param dat data.frame, with col: T, A, C, W
+#' @param T.uniq vector of unique event times
+#' @param ht.SL.Lib library for SuperLearner
+#'
+#' @return h.hat.t matrix, nrow = n.sample, ncol = length(T.uniq);
+#'          each row is the predicted conditional hazard h(T=t | T>=t, A, W) for that subject
+#' @export
+#'
+#' @examples
+#' # TO DO
+#' @import dplyr
+#' @import tidyr
+estimate_hazard_SL <- function(dat,
+                           T.uniq,
+                           ht.SL.Lib = c("SL.mean","SL.glm", "SL.gam", "SL.earth")
+                           ) {
+    # transform original data into SL-friendly format
+    dat_david <- dat
+
+    dat_david <- rename(dat_david, Z = A)
+
+    if ('ID' %in% toupper(colnames(dat_david))) {
+        # if there are already id in the dataset
+        dat_david <- rename(dat_david, id = ID)
+    }else{
+        # if no id exist
+        # create 'id' on our own
+        dat_david$id <- 1:nrow(dat_david)
+    }
+
+    # censoring
+    dat_david <- rename(dat_david, Delta.J = delta)
+
+    # remove all other useless columns
+    baseline_name <- grep('W', colnames(dat_david), value = TRUE)
+    keeps <- c("id", baseline_name, 'T.tilde', 'Delta.J', 'Z')
+    dat_david <- dat_david[,keeps]
+
+    # first fit SL on the max time point
+    T.it <- max(T.uniq)
+    # =======================================================================================
+    # dataList
+    # =======================================================================================
+    datalist <- makeDataList(dat = dat_david,
+                             J = 1, # one kind of failure
+                             nZ = 2, # one kind of treatment
+                             Z = c(0,1),
+                             t0 = T.it, # time to predict on
+                             bounds=NULL)
+    # ----------------------------------------------------------------------------------------------------
+    # perform hazard SL for the maximum time point
+    Haz_hat <- estimateHazards(dataList = datalist, J=1, verbose=FALSE, strata=NULL,
+                               adjustVars = dat_david[,baseline_name],
+                               SLlibrary.event = ht.SL.Lib,
+                               glmFormula.event = NULL,
+                               bounds = NULL
+    )
+    # Haz_hat[[1]][Haz_hat[[1]]$t == T.it,'Q1Haz']
+    # mean(Haz_hat[[1]][Haz_hat[[1]]$t == T.it,'Q1Haz'])
+
+    # turn into wide format
+
+    out_haz <- Haz_hat[[3]]
+    out_haz <- out_haz[,c('id', 't', 'Q1Haz')]
+    out_haz <- spread(out_haz, t, Q1Haz)
+    # the colname number correspond to h_{T>t-1 | T>=t-1}
+    rownames(out_haz) <- out_haz$id
+
+    # turn NA entries (after failure) into zero hazard
+    out_haz[is.na(out_haz)] <- 0
+
+    # remove the id column
+    out_haz_2 <- out_haz[,-1]
+
+    # subset the columns for those only in T.uniq
+    out_haz <- out_haz_2[,T.uniq]
+
+    return(list(out_haz = out_haz,
+                out_haz_full = out_haz_2))
+}
+
+
+
+#' wrapper for censoring regression SuperLearner
+#'
+#' @param dat data.frame, with col: T, A, W, Delta
+#' @param T.uniq the unique time points of failure
+#' @param Delta.SL.Lib library for SuperLearner
+#'
+#' @return h.hat.t matrix, nrow = n.sample, ncol = length(T.uniq);
+#' each row is the predicted conditional hazard h(T=t | T>=t, A, W) for that subject
+#' @export
+#'
+#' @examples
+#' # TO DO
+#' @import SuperLearner
+#' @import survtmle
+#' @import dplyr
+#' @import tidyr
+estimate_censoring_SL <- function(dat,
+                              T.uniq,
+                              Delta.SL.Lib = c("SL.mean","SL.glm", "SL.gam", "SL.earth")
+                              ) {
+    # transform original data into SL-friendly format
+    dat_david <- dat
+
+    dat_david <- rename(dat_david, ftime = T.tilde)
+    dat_david <- rename(dat_david, trt = A)
+
+    if ('ID' %in% toupper(colnames(dat_david))) {
+        # if there are already id in the dataset
+        dat_david <- rename(dat_david, id = ID)
+    }else{
+        # if no id exist
+        # create 'id' on our own
+        dat_david$id <- 1:nrow(dat_david)
+    }
+
+    # censoring
+    if ('delta' %in% colnames(dat_david)) {
+        dat_david <- rename(dat_david, ftype = delta)
+    }
+
+    # remove all other useless columns
+    baseline_name <- grep('W', colnames(dat_david), value = TRUE)
+    keeps <- c("id", baseline_name, 'ftime', 'ftype', 'trt')
+    dat_david <- dat_david[, keeps]
+
+    T.uniq <- unique(sort(dat_david$ftime))
+    T.max <- max(T.uniq)
+
+    adjustVars <- dat_david[,baseline_name]
+    # ====================================================================================================
+    # dataList
+    # ====================================================================================================
+    datalist <- survtmle:::makeDataList(dat = dat_david,
+                                       J = 1, # one kind of failure
+                                       ntrt = 2, # one kind of treatment
+                                       uniqtrt = c(0,1),
+                                       t0 = T.max, # time to predict on
+                                       bounds=NULL)
+    # ====================================================================================================
+    # estimate g_2 (censoring)
+    # perform censoring SL for the maximum time point
+    # ====================================================================================================
+    g2_hat <- survtmle:::estimateCensoring(dataList = datalist, adjustVars = adjustVars,
+                                t0 = T.max,
+                                ntrt = 2, # one kind of treatment
+                                uniqtrt = c(0,1),
+                                SL.ctime = Delta.SL.Lib,
+                                returnModels = FALSE,verbose = FALSE)
+    dataList2 <- g2_hat$dataList
+    # ----------------------------------------------------------------------------------------------------
+    # turn into wide format
+    out_censor <- dataList2$`1`
+    out_censor <- out_censor[,c('id', 't', 'G_dC')]
+    out_censor <- spread(out_censor, t, G_dC)
+    # the colname number correspond to h_{T>t-1 | T>=t-1}
+    rownames(out_censor) <- out_censor$id
+
+    # turn NA entries (after failure) into zero hazard
+    out_censor[is.na(out_censor)] <- 0
+
+    # remove the id column
+    out_censor_2 <- out_censor[,-1]
+
+    # subset the columns for those only in T.uniq
+    out_censor <- out_censor_2[,T.uniq]
+
+    return(list(out_censor = out_censor,
+                out_censor_full = out_censor_2))
+}
+
+
 #' generate data.frame to be used in survtmle package
 #'
 #' @param dat a data.frame with columns named:
@@ -89,6 +267,7 @@ makeDataList <- function(dat, J, nZ, Z, t0, bounds=NULL){
 }
 
 #' Conditional Hazard estimation in survtmle package
+#' From survtmle (David Benkeser, 2016)
 #'
 #' @param dataList output of makeDataList
 #' @param J same as above
